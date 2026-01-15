bd755bbccf07940682568f18ae9923db10930828
René Schwe. <106735291+schweRene@users.noreply.github.com>
2026-01-15 20:52:00 +0100

    Update mobile_app.py

--- mobile_app.py
+++ mobile_app.py
@@ -6,9 +6,9 @@ from sqlalchemy import text
 def show_mobile_startseite():
     # Überschrift zentriert mit Abstand nach unten
     st.markdown("""
-        <h2 style='text-align: center; color: #8B0000; margin-bottom: 30px;'>
+        <h3 style='text-align: center; color: #8B0000; margin-bottom: 30px;'>
             ⚽ Bundesliga-Dashboard
-        </h2>
+        </h3>
     """, unsafe_allow_html=True)
     
     if os.path.exists("bundesliga.jpg"):
