983b1305a70e4981b59c23abd916d84a99972e35
schweRene <schwemerrene@gmail.com>
2026-01-19 14:20:51 +0100

    Ansichtenwechsel konfiguriert

--- router.py
+++ router.py
@@ -4,11 +4,13 @@ st.set_page_config(page_title="Bundesliga Dashboard", layout="wide")
 
 from streamlit_javascript import st_javascript
 
-# Falls die Breite schon bekannt ist, laden wir direkt
-if "device_width" not in st.session_state:
-    st.session_state.device_width = None
+
 
 def start_router():
+    # Session State initialisieren
+    if "device_width" not in st.session_state:
+        st.session_state.device_width = None
+
     # Breite nur abfragen, wenn wir sie noch nicht haben
     if st.session_state.device_width is None:
         width = st_javascript("window.innerWidth")
@@ -17,11 +19,11 @@ def start_router():
             st.rerun()
         else:
             # Während er wartet, zeigen wir kurz was an
-            st.write("Verbinde mit Server...")
+            st.info("Layout wird geladen")
             return
 
     # Jetzt entscheiden wir basierend auf dem gespeicherten Wert
-    if st.session_state.device_width < 800:
+    if st.session_state.device_width < 1000:
         import mobile_app
         mobile_app.run_mobile_main()
     else:
